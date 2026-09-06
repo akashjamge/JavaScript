e6769eb57cb184543c69910ed9bace3936178f61
akashjamge <akashgj242001@gmail.com>
2026-09-06 06:50:59 +0000

    Comparision of datatypes

---
+++ .devcontainer/01_Basics/04_ComparisionOfDatatypes.js
@@ -0,0 +1,28 @@
+// console.log(2>10);
+// console.log(2>=10);
+// console.log(2<10);
+// console.log(2<=10);
+// console.log(2==10);
+// console.log(2!=10);
+
+// console.log("2" > 1);
+// console.log("1" > 2);
+// console.log("02" > 5);
+
+
+// == works differently and >= <= works differently
+
+//for null
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+                                 //Avoid these type of conversions
+//for undefined
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+console.log("2" === 2);
+
+
