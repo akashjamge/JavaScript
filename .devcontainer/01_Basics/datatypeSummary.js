b86afd5e08660c1a71f16bc163abbd26d52ada83
akashjamge <akashgj242001@gmail.com>
2026-09-07 05:07:50 +0000

    DatatypeSummary

--- .devcontainer/01_Basics/datatypeSummary.js
+++ .devcontainer/01_Basics/datatypeSummary.js
@@ -33,10 +33,10 @@ let userEmail ;//let userName = undefined
 //Symbol is use to make component un unique
 const Id = Symbol("123")
 const anotherId =Symbol("123") 
-console.log(Id === anotherId);
+// console.log(Id === anotherId);
 
 const bigNumber = 564544568454n//If we want to represent any number as BigInt just add n at the end
-console.log(typeof bigNumber);//return type is undefined
+// console.log(typeof bigNumber);//return type is undefined
 
 // 2 .Non primitive datatype(reference type)
 //Return type of all non primitive datatype is object and of function is object function
@@ -44,7 +44,7 @@ console.log(typeof bigNumber);//return type is undefined
 
 //Array
 const Heroes =["Hulk","Thor","Shaktimaan"];
-console.log(typeof Heroes);
+// console.log(typeof Heroes);
 
 //Object (Declared inside {})
 let myObj = {
@@ -57,5 +57,40 @@ let myObj = {
 let myFunction = function (){
     //console.log("hello");
 }
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+//***************************************************** */
+
+
+// Stack and Heap Memory
+// 1. Primitive datatypes are stored in stack 
+// 2. Non primitive datatypes are stored in Heap memory
+
+//Ex-primitive datatype
+// In stack it creates another copy 
+let myName ="Akash"
+let anotherName = "Babloo"
+
+anotherName = myName
+console.log(myName);
+console.log(anotherName);
+
+
+//Ex-Non primitive datatype
+// It will point to the reference of object in heap
+let user1 = {
+    email : "Akash@mail.com",
+    upi : "808070@ybl"
+}
+
+let user2 = user1
+
+user2.email = "aj@gmail.com"
+
+
+console.log(user1.email);
+console.log(user2.email);
+
+
+
 
