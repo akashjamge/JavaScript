2f0bfb851fb63534c55147e6c18824dc44823cbb
akashjamge <akashgj242001@gmail.com>
2026-09-08 05:57:04 +0000

    Number and Math

---
+++ .devcontainer/01_Basics/06_NumbersAndMath.js
@@ -0,0 +1,42 @@
+const score = 100
+// console.log(score);
+
+
+//Declaring number explicitly with the help of new keyword (object creation)
+const balance = new Number(500)
+// console.log(balance);
+ 
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const num1 = 123.8966
+// console.log(num1.toPrecision(4));
+
+const num2 = 100000000
+// console.log(num2.toLocaleString('en-IN'));
+
+
+//******************************* Maths ************************ */
+
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(9.9));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.6));
+// console.log(Math.min(1,3,45,6));
+// console.log(Math.max(2,3,45,6));
+
+
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10 
+const max = 20 
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
+
+
+
+
