bedf63d62edab2b6359634016fefbe07d36345a5
akashjamge <akashgj242001@gmail.com>
2026-09-16 06:07:12 +0000

    scope level and mini hoisting

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -19,8 +19,62 @@ var c = 300
     const b = 20 
     var c = 30
 }
-console.log(a);
-console.log(b);
-console.log(c);//30
+// console.log(a);
+// console.log(b);
+// console.log(c);//30
 
 // console madhla global scope and node use karun access kelela global scope donhi alag aahet
+
+
+
+// scope level and mini hoisting 
+
+function one(){
+    const username = "Akash"
+
+    function two(){
+        const website = "Youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+    
+}
+// console.log(username);
+// one()
+
+
+
+if (true) {
+    const username = "Akash"
+
+    if (username === "Akash") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// *********************** Interesting *******************************
+
+// if we only declare the function without storing 
+// it in any variable we can access theat function
+// before declaration
+
+// console.log(addOne(5));
+function addOne(num){
+    return num+1
+}
+
+
+// Hoisting - 
+// we can not access the variable or function before the intialization when 
+// the function is wrapped or declared inside the variable
+
+// console.log(addTwo(6));//Error
+const addTwo = function(num){
+    return num+2
+}
+
